2e3d2269da9b2b7fbae55734a86490f039c4c651
rafael <rafael.19.rosendo@gmail.com>
2025-02-23 18:38:31 -0300

    falta a main e testes de eficiência

---
+++ bTree/bAux.cpp
@@ -0,0 +1,158 @@
+#include <cstddef>
+#include <iostream>
+#include <algorithm>
+#include "header.hpp"
+#include <memory>
+#include <string>
+
+
+Tree::Tree(const Node &no): no(no) , num_filhos(0), esq(nullptr), dir(nullptr){}
+
+
+Tree::~Tree(){}
+
+Tree * Tree::arvore_criaArv(){
+    return nullptr;
+}
+
+
+Node * Tree::arv_CriaNo(Node no){
+    return new Node(no.code , no.score , no.cpf , no.nome);
+}
+
+
+void  Tree::arv_insere(Tree *raiz , Node no){
+    if(raiz == nullptr){
+        raiz = new Tree(no);
+        std::cout<<"\n\n Nó criado com sucesso! \n\n";
+        return;
+    }
+
+    if(no.score < raiz -> no.score){
+        if(!raiz -> esq){
+            raiz -> esq = std::make_unique<Tree>(no);
+            std::cout<<"\n\n Nó criado com sucesso! \n\n";
+
+        }
+        else{
+            arv_insere(raiz -> esq.get() , no);
+        }
+    }
+    else if(no.score > raiz -> no.score){
+        if(!raiz -> dir){
+            raiz -> dir = std::make_unique<Tree>(no);
+            std::cout<<"\n\n Nó criado com sucesso! \n\n";
+        } else{
+            arv_insere(raiz -> dir.get() , no);
+        }
+    }
+
+    else{
+        std::cout<<"\n\n Nó já existe! \n\n";
+    }
+}
+
+Tree * Tree::arv_buscaNode(Tree * raiz , int codigo){
+
+    if(raiz == nullptr || raiz -> no.code == codigo){
+        std::cout<< "Arvore vazia meu caro \n\n";
+        return raiz;
+    }
+
+    if(codigo < raiz -> no.code){
+        return arv_buscaNode(raiz -> esq.get() , codigo);
+    }
+    else{
+        return arv_buscaNode(raiz -> dir.get(),  codigo);
+    }
+}
+
+
+bool  Tree::arv_vazia(Tree * raiz){
+   return (raiz == nullptr) ? true : false ;
+}
+
+bool Tree::arv_removeNode(std::unique_ptr<Tree> &raiz , int code){
+
+    if( !raiz ){
+        std::cout<< "arvore vazia, nada a remover ";
+        return false;
+    }
+
+    if( code < raiz -> no.code ){
+        return arv_removeNode( raiz -> esq ,  code);
+    }
+    else if(code > raiz ->no.code){
+        return arv_removeNode(raiz -> dir ,  code);
+    }
+
+    //entrou no nó específico
+       //sem filhos
+      if( !raiz -> esq  && !raiz -> dir ){
+
+          raiz.reset();
+          return true;
+      }
+
+      //tem pelo menos um filho
+      if(!raiz -> esq){
+        raiz =  std::move(raiz -> dir);
+        return true;
+      }
+
+      if(!raiz -> dir){
+          raiz = std::move(raiz -> esq);
+          return true;
+      }
+
+
+      Tree * aux = raiz -> dir.get();
+
+      while(aux -> esq){
+          aux = aux -> esq.get();
+      }
+
+    raiz -> no = aux -> no;
+    return arv_removeNode(raiz ->dir ,  code);
+}
+
+void Tree::arv_imprimeOrdem(Tree *raiz){
+    if(!raiz){ return; }
+
+    arv_imprimeOrdem(raiz -> esq.get());
+    std::cout << "Code: " << raiz->no.code
+                  << " , Score: " << raiz->no.score
+                  << " , CPF: " << raiz->no.cpf
+                  << " , Nome: " << raiz->no.nome << std::endl;
+
+
+    arv_imprimeOrdem(raiz -> dir.get());
+}
+
+void Tree::arv_imprimePosOrdem(Tree *raiz){
+    if (!raiz) {return; }
+
+    arv_imprimePreOrdem(raiz -> esq.get());
+    arv_imprimePreOrdem(raiz -> dir.get());
+
+    std::cout << "Code: " << raiz->no.code
+                  << " , Score: " << raiz->no.score
+                  << " , CPF: " << raiz->no.cpf
+                  << " , Nome: " << raiz->no.nome << std::endl;
+
+}
+
+void Tree::arv_imprimePreOrdem(Tree *raiz){
+    if (!raiz) {return ;}
+
+
+
+    std::cout << "Code: " << raiz->no.code
+                  << " , Score: " << raiz->no.score
+                  << " , CPF: " << raiz->no.cpf
+                  << " , Nome: " << raiz->no.nome << std::endl;
+
+
+    arv_imprimePreOrdem(raiz -> esq.get());
+    arv_imprimePreOrdem(raiz -> dir.get());
+}
---
+++ bTree/header.hpp
@@ -0,0 +1,51 @@
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+#include <cstdio>
+#include <memory>
+#include <string>
+
+
+class Node{
+    public:
+   int code;
+   int score;
+   std::string cpf;
+   std::string nome;
+
+   Node(int code, int score , std::string cpf , std::string nome): code(code), score(score), cpf(cpf), nome(nome){}
+   };
+
+
+class Tree{
+    private:
+        std::unique_ptr<Tree> esq;
+        std::unique_ptr<Tree> dir;
+        Node no;
+        int num_filhos;
+    public:
+        explicit Tree(const Node& no);
+        ~Tree();
+
+        //Escrita de métodos a seguir somente as assinaturas
+
+        Tree * arvore_criaArv(); //feito
+        void  arv_insere(Tree * raiz , Node no); //feito
+        Node * arv_CriaNo(Node no); //feito
+        Tree * arv_buscaNode(Tree * raiz , int code); //feito
+
+        bool arv_vazia(Tree * raiz); //feito
+        bool arv_removeNode(std::unique_ptr<Tree> &raiz , int code); //feito
+
+        void arv_imprimeOrdem(Tree * raiz);
+        void arv_imprimePreOrdem(Tree * raiz);
+        void arv_imprimePosOrdem(Tree * raiz);
+
+        int arv_altura(Tree * raiz);
+
+
+
+};
+
+
+#endif
